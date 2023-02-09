83923bfbe1253842d5eecef81226b3a2e335d4c4
jwaddell10 <jewaddell91@gmail.com>
2023-02-09 16:17:37 -0600

    deleted old code

--- script.js
+++ script.js
@@ -43,31 +43,7 @@ function clearGrid() {
     });
 }
 clearGrid()
-//function to updatesize of squares when new grid is created
-/* function updateSize() {
-    let userInput = document.querySelector("#userInput");
-    console.log(userInput);
-    for (let i = 0; i < userInput.value * userInput.value; i++) {
-        const squares = document.createElement('div')
-        container.appendChild(squares);
-        squares.classList.add('grid');
 
-        squares.innerHTML = "";
-        squares.style.removeProperty('border');
-        squares.style.setProperty(
-            "grid-template-columns",
-            `repeat(${userInput.value}, 2fr)`
-        );
-        squares.style.setProperty(
-            "grid-template-rows",
-            `repeat(${userInput.value}, 2fr)`
-        );
-    }
-}
-
-updateSize()*/
-
-//function to get userInput when number is entered into prompt
 function getUserInput() {
     let removeBtn = document.querySelector("#remove");
     removeBtn.addEventListener("click", () => {
@@ -84,54 +60,3 @@ function getUserInput() {
 }
 
 getUserInput();
-/*function makeDivs(numDivs) {
-    for (let i = 0; i < numDivs; i++) {
-        let cells = document.createElement('div');
-        container.appendChild(cells)
-        cells.classList.add('grid')
-        cells.style.width = "30px";
-    }
-}
-
-makeDivs(256);
-
-function hover() {
-    let elements = document.getElementsByClassName("grid");
-
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("mouseover", () => {
-            elements[i].style.backgroundColor = "black";
-        })
-    }
-}
-hover()
-
-
-function size() {
-
-    const gridSize = document.getElementById('userInput').value;
-    const element = document.querySelectorAll('.grid');
-    element.remove()
-    makeDivs(gridSize);
-}
-size()*/
-/*
-function gridSize() {
-
-    const ten = document.getElementById("10");
-    ten.addEventListener("click", () => {
-        makeDivs(100);
-        hover()
-    })
-    const twenty = document.getElementById("20");
-    twenty.addEventListener("click", () => {
-        makeDivs(400);
-        hover()
-    })
-    const thirty = document.getElementById("30");
-    thirty.addEventListener("click", () => {
-        makeDivs(895);
-        hover()
-    })
-}
-gridSize(); */
